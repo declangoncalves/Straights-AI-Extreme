2432907e5ea883c34efb662938724cb06ad03497
declan1397 <declan.gon@gmail.com>
2017-06-30 22:05:09 -0500

    Big View Update

--- view.cc
+++ view.cc
@@ -3,7 +3,9 @@
 #include "controller.h"
 #include "model.h"
 #include "subject.h"
+#include "Card.h"
 #include <iostream>
+
 
 View::View(Controller* c, Model* m) {
 	controller_ = c;
@@ -11,3 +13,111 @@ View::View(Controller* c, Model* m) {
 }
 
 View::~View() {}
+
+void update() override {
+
+	if (gamePhase == 1) { // Start Round
+		roundStart();
+	}
+
+	else if (gamePhase == 3) { // Start new round
+		gamePhase = 1;
+		roundEnd();
+	}
+
+	else {
+		playerTurn();
+	}
+}
+
+void roundStart() {
+	cout << "A	new	round	begins.	It’s	player	" << model_->getCurrentPlayerIndex() + 1 << "’s	turn	to	play.\n";
+	gamePhase = 2;
+	playerTurn();
+}
+
+void playerTurn() {
+	std::vector<Cards> cardTable = model->getCardTable();
+	std::vector<int> intTable = model->getIntTable();
+	std::vector<int> legalPlays = model->getLegalPlays();
+
+	cout << "Cards	on	the	table:\n";
+	cout << "\nClubs:";
+	printClubs();
+	cout << "\nDiamonds:";
+	printDiamonds();
+	cout << "\nHearts:";
+	printHearts();
+	cout << "\nSpades:";
+	printSpades();
+	cout << "\nYour	hand:"
+	printPlayerHand(cardTable, intTable);
+	cout << "\nLegal	plays:"
+	printLegalPlays(legalPlays);
+
+}
+
+// Print Legal Plays
+void printLegalPlays(std::vector<Cards> legalPlays) {
+	for (int i = 0; i < legalPlays.size(); i++) {
+		cout << legalPlays[i];
+	}
+}
+
+// Print Player Hand
+void printPlayerHand(std::vector<Cards> cardTable, std::vector<Cards> intTable) {
+	Player p = model->getCurrentPlayer();
+	for (int i = 0; i < p.getHand().size(); i++) {
+		cout << p[i];
+	}
+}
+
+// Print Clubs
+void printClubs(std::vector<Cards> cardTable, std::vector<Cards> intTable) {
+	for (int i = 1; i < 14; i++) {
+		if (intTable[0][i] == 1) {
+			cout << rankToLetter(cardTable[0][i].rank());
+		}
+	}
+}
+
+// Print Diamonds
+void printDiamonds(std::vector<Cards> cardTable, std::vector<Cards> intTable) {
+	for (int i = 1; i < 14; i++) {
+		if (intTable[1][i] == 1) {
+			cout << rankToLetter(cardTable[0][i].rank());
+		}
+	}
+}
+
+// Print Hearts
+void printHearts(std::vector<Cards> cardTable, std::vector<Cards> intTable) {
+	for (int i = 1; i < 14; i++) {
+		if (intTable[2][i] == 1) {
+			cout << rankToLetter(cardTable[0][i].rank());
+		}
+	}
+}
+
+// Print Spades
+void printSpades(std::vector<Cards> cardTable, std::vector<Cards> intTable) {
+	for (int i = 1; i < 14; i++) {
+		if (intTable[3][i] == 1) {
+			cout << rankToLetter(cardTable[0][i].rank());
+		}
+	}
+}
+
+// Return character rank
+char rankToLetter(int rank) {
+	switch (rank) {
+	case 1: return 'A';
+	case 10: return 'T';
+	case 11: return 'J';
+	case 12: return 'Q';
+	case 13: return 'K';
+	case default: return "" + rank;
+	}
+}
+
+void roundEnd();
--- view.h
+++ view.h
@@ -10,11 +10,25 @@ class View : public Observer {
   public:
     View( Controller*, Model* );
     virtual ~View();
-    virtual void update();
+    void update() override;
+
+	void roundStart();
+	void playerTurn();
+	void roundEnd();
+
+	// Helper Functions
+	const void printClubs(std::vector<Cards>;
+	const void printSpades(std::vector<Cards>;
+	const void printHearts(std::vector<Cards>;
+	const void printDiamonds(std::vector<Cards>;
+
+	const void printPlayerHand(std::vector<Cards>;
+	const void printLegalPlays(std::vector<Cards>;
 
   private:
     Model *model_;
     Controller *controller_;
+	int gamePhase_ = 1; // 1 - RoundStart | 2 - PlayerTurn | 3 - RoundEnd
 }; // View
 
 #endif
