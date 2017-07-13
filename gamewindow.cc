454e51303984b9182877985de8cbbd3734752514
Danyal Raza <danyal.raza@vidyard.com>
2017-07-13 17:28:39 -0400

    start

--- gamewindow.cc
+++ gamewindow.cc
@@ -22,12 +22,12 @@ GameWindow::GameWindow(const Glib::RefPtr<Gtk::Application>& app, Controller* c,
     model_ = m;
     model_->subscribe(this);
     add(m_Box);
-    m_refBuilder->get_widget("start_game_btn", start_game_btn);
-    if (!start_game_btn) {
+    m_refBuilder->get_widget("glade_window", glade_window);
+    if (!glade_window) {
       std::cout << "this didn't work" << std::endl;
     }
     else {
-      m_Box.pack_start(*start_game_btn, Gtk::PACK_SHRINK);
+      m_Box.pack_start(*glade_window, Gtk::PACK_SHRINK);
     }
     m_refBuilder->get_widget("end_game_btn", end_game_btn);
     m_refBuilder->get_widget("p1_RQ", p1_RQ);
