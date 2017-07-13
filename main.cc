0226686d6669bc5eef9a62b6469cdebcd023aade
declan1397 <declan.gon@gmail.com>
2017-07-12 21:05:46 -0500

    This should do it

--- main.cc
+++ main.cc
@@ -28,10 +28,13 @@ int main( int argc, char * argv[] ) {
       players.push_back(a);
   }
 
+  app->run(window);
+  
+
   Model model(seed, players); // Create model
   Controller controller( &model ); // Create controller
   View view( &controller, &model); // Create the view
 
-  return app->run(window);
+  return 0;
 
 } // main
