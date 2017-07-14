bb16198065d4f8691495ce24b591635fde623f04
Danyal Raza <danyal.raza@vidyard.com>
2017-07-14 00:27:10 -0400

    holy shit

--- gamewindow.cc
+++ gamewindow.cc
@@ -115,7 +115,7 @@ void GameWindow::handClicked(int i) {
 
 void GameWindow::startGame() {
   int seed = 0;
-  char* seed_str = gtk_entry_get_text(GTK_ENTRY(seed_input));
+  gchar* seed_str = gtk_entry_get_text(GTK_ENTRY(seed_input));
   seed = atoi(seed_str);
 
   try
