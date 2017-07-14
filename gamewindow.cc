c1d35c5871957e0fdddeaf2bd6a36c1fe64ba18a
declan1397 <declan.gon@gmail.com>
2017-07-14 01:15:47 -0500

    Lets go

--- gamewindow.cc
+++ gamewindow.cc
@@ -46,9 +46,9 @@ GameWindow::GameWindow(const Glib::RefPtr<Gtk::Application>& app, Controller* c,
     refBuilder->get_widget("hand_btn_13", handButtons_[12]);
 
     for (auto button : handButtons_){
-      Gtk::Widget* image = gtk_button_get_image(button);
+      Gtk::Widget* image = gtk_button_get_image(button).Image();
       gtk_image_set_from_file(image, "./img/nothing.png");
-      gtk_button_set_image(button, image);
+      //gtk_button_set_image(button, image);
     }
 
     start_game_btn->signal_clicked().connect(sigc::mem_fun(*this, &GameWindow::startGame));
