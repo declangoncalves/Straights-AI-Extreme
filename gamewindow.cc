3e2b5d568e1a9b99f1cc40505745f30a30778f27
Danyal Raza <danyal.raza@vidyard.com>
2017-07-14 15:02:49 -0400

    huehue choices

--- gamewindow.cc
+++ gamewindow.cc
@@ -118,7 +118,7 @@ void GameWindow::startGame() {
   seed = std::stoi(seed_str);
   std::cout << "this worked with seed: " << seed <<  std::endl;
 
-  controller_->startGame(seed, chocies_);
+  controller_->startGame(seed, choices_);
   try
   {
     refBuilder->add_from_file("glade_project.glade");
